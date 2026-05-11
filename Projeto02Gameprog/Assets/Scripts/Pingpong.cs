16daa844e7b15109933fc50bd73eb0629b88cb5d
AllanSTrab <pekagamestrabbold@gmail.com>
2026-05-11 11:38:52 -0300

    aula12

--- Projeto02Gameprog/Assets/Scripts/Pingpong.cs
+++ Projeto02Gameprog/Assets/Scripts/Pingpong.cs
@@ -1,22 +1,50 @@
-using System;
 using UnityEngine;
-using UnityEngine.InputSystem.Processors;
-using UnityEngine.InputSystem.XR.Haptics;
 
 public class Pingpong : MonoBehaviour
 {
     [SerializeField] float velocidade = 10.0f;
+    [SerializeField] float velocidadeRot = 10f;
     void Update()
     {
+        //Movimento
+
         float mover_x = Input.GetAxisRaw("Horizontal") * velocidade * Time.deltaTime;
         float mover_y = Input.GetAxisRaw("Vertical") * velocidade * Time.deltaTime;
-        transform.Translate(mover_x, 0f, 0f);
-        transform.Translate(0f, mover_y, 0f);
-        //transform.Rotate(0.0f, 0.0f 1.0f * Time.deltaTime)
-        //COM ACELERA플O
-        //print(Input.GetAxis("Vertical"));
-        //print(Input.GetAxis("Horizontal"));
-        //SEM ACELERA플O
-        //print(Input.GetAxisRaw("Horizontal");
+        transform.Translate(mover_x, mover_y, 0f);
+
+        //Tiros
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //Retorna o valor para a coordenada indicada
+            transform.position = new Vector2 (0f, 0f);
+            transform.eulerAngles = new Vector3 (0f, 0f, 0f);
+        }
+
+        if (Input.GetButton("Fire2"))
+        {
+            transform.Rotate(0f, 0f, velocidadeRot * Time.deltaTime);
+            GetComponent<SpriteRenderer>().color = Color.red;
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            GetComponent<SpriteRenderer>().color = Color.yellow;
+        }
+        if ()
+        /*else if (Input.GetButton("Fire1"))
+        {
+            print("Button");
+        }
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            print("Up");
+        }
+        transform.Rotate(0.0f, 0.0f 1.0f * Time.deltaTime)
+        COM ACELERA플O
+        print(Input.GetAxis("Vertical"));
+        print(Input.GetAxis("Horizontal"));
+        SEM ACELERA플O
+        print(Input.GetAxisRaw("Horizontal");*/
     }
 }
